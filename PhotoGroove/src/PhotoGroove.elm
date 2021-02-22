6ab77f523af0c5381ee5c605b799877cf55d6acc
Jeremy Dobbins-Bucklad <jeremy.dobbins.bucklad@oracle.com>
2021-02-22 17:05:39 -0500

    Next: 3.2.3

--- PhotoGroove/src/PhotoGroove.elm
+++ PhotoGroove/src/PhotoGroove.elm
@@ -1,70 +1,124 @@
+-- Next: 3.2.3
+
+
 module PhotoGroove exposing (main)
 
-import Browser
 import Array exposing (Array)
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
+
 urlPrefix : String
-urlPrefix = 
-  "http://elm-in-action.com/" 
+urlPrefix =
+    "http://elm-in-action.com/"
 
-type alias Photo = 
-  { url : String }
 
-type alias Model = 
-  { photos : List Photo
-  , selectedUrl : String 
-  }
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    , chosenSize : ThumbnailSize
+    }
+
+
+type alias Msg =
+    { description : String, data : String }
+
 
 initialModel : Model
-initialModel = 
-  { photos = 
-      [ { url = "1.jpeg" }
-      , { url = "2.jpeg" }
-      , { url = "3.jpeg" }
-      ]
-  , selectedUrl = "1.jpeg"
-  }
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    , chosenSize = Large
+    }
+
 
 photoArray : Array Photo
-photoArray = Array.fromList initialModel.photos
+photoArray =
+    Array.fromList initialModel.photos
 
-type alias Msg = 
-  { description : String, data : String }
 
 view : Model -> Html Msg
-view model = 
-  div [ class "content" ]
-      [ h1 [] [ text "Photo Groove"]
-      , div [ id "thumbnails" ] 
-          (List.map (viewThumbnail model.selectedUrl) model.photos)
-      , img
-          [ class "large"
-          , src (urlPrefix ++ "large/" ++ model.selectedUrl)
-          ]
-          []
-      ]
+view model =
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , button
+            [ onClick { description = "ClickedSurpriseMe", data = "" } ]
+            [ text "Surprise Me!" ]
+        , h3 [] [ text "Thumbnail Size:" ]
+        , div [ id "choose-size" ]
+            (List.map viewSizeChooser [ Small, Medium, Large ])
+        , div [ id "thumbnails", class (sizeToString model.chosenSize) ]
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
+        ]
+
 
 viewThumbnail : String -> Photo -> Html Msg
-viewThumbnail selectedUrl thumb = 
-  img 
-    [ src (urlPrefix ++ thumb.url) 
-    , classList [ ( "selected", selectedUrl == thumb.url ) ]  
-    , onClick { description = "ClickedPhoto", data = thumb.url }
-    ] 
-    []
-  
-update msg model =
-  if msg.description == "ClickedPhoto" then
-    { model | selectedUrl = msg.data }
-  else
-    model
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick { description = "ClickedPhoto", data = thumb.url }
+        ]
+        []
 
-main = 
-  Browser.sandbox
-    { init = initialModel
-    , view = view
-    , update = update
-    }
+
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input [ type_ "radio", name "size" ] []
+        , text (sizeToString size)
+        ]
+
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Large ->
+            "large"
+
+        Medium ->
+            "med"
+
+        Small ->
+            "small"
+
+
+update msg model =
+    case msg.description of
+        "ClickedPhoto" ->
+            { model | selectedUrl = msg.data }
+
+        "ClickedSurpriseMe" ->
+            { model | selectedUrl = "2.jpeg" }
+
+        _ ->
+            model
+
+
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
