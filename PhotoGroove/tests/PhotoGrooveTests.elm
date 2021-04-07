f7806e4e4cd091a8d02203f0acfe097bf57dbfec
Jeremy Dobbins-Bucklad <jad-b@users.noreply.github.com>
2021-04-07 06:08:13 -0400

    @6.3.3

--- PhotoGroove/tests/PhotoGrooveTests.elm
+++ PhotoGroove/tests/PhotoGrooveTests.elm
@@ -5,7 +5,17 @@ import Fuzz exposing (Fuzzer, int, list, string)
 import Html.Attributes as Attr exposing (src)
 import Json.Decode as D
 import Json.Encode as E
-import PhotoGroove exposing (Model, Msg(..), Photo, initialModel, update)
+import PhotoGroove
+    exposing
+        ( Model
+        , Msg(..)
+        , Photo
+        , Status(..)
+        , initialModel
+        , update
+        , urlPrefix
+        , view
+        )
 import Test exposing (..)
 import Test.Html.Query as Query
 import Test.Html.Selector exposing (attribute, tag, text)
@@ -103,3 +113,35 @@ noPhotosNoThumbnails =
                 |> Query.findAll [ tag "img" ]
                 -- (Int -> Expectation) -> Query.Multiple Msg -> Expectation
                 |> Query.count (Expect.equal 0)
+
+
+thumbnailRendered : String -> Query.Single msg -> Expectation
+thumbnailRendered url query =
+    query
+        |> Query.findAll [ tag "img", attribute (Attr.src (urlPrefix ++ url)) ]
+        |> Query.count (Expect.atLeast 1)
+
+
+photoFromUrl : String -> Photo
+photoFromUrl url =
+    { url = url, size = 0, title = "" }
+
+
+thumbnailsWork : Test
+thumbnailsWork =
+    fuzz (Fuzz.intRange 1 5) "URLs render as thumbnais" <|
+        \urlCount ->
+            let
+                urls : List String
+                urls =
+                    List.range 1 urlCount
+                        |> List.map (\num -> String.fromInt num ++ ".png")
+
+                thumbnailChecks : List (Query.Single msg -> Expectation)
+                thumbnailChecks =
+                    List.map thumbnailRendered urls
+            in
+            { initialModel | status = Loaded (List.map photoFromUrl urls) "" }
+                |> view
+                |> Query.fromHtml
+                |> Expect.all thumbnailChecks
